11d5657fa90064a4c9f8771d882f5d749822b363
ssangbomb <ssangbomb@gmail.com>
2023-05-06 16:39:11 -0700

    add table query

---
+++ db/schema/03_comments.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS comments CASCADE;
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
+  message TEXT
+)
