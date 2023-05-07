f28212012b425339cd108d5b36ff9a0767157886
Kryxsoo <MattCharmZ@gmail.com>
2023-05-06 18:40:24 -0700

    Add Comments seeds

---
+++ db/seeds/03_comments.sql
@@ -0,0 +1,5 @@
+INSERT INTO comments (user_id, post_id, message) (1, 1, "Hi this is a text comment");
+INSERT INTO comments (user_id, post_id, message) (2, 2, "Hi this is a text comment3");
+INSERT INTO comments (user_id, post_id, message) (3, 3, "Hi this is a text comment4");
+INSERT INTO comments (user_id, post_id, message) (2, 1, "Hi this is a text comment2");
+INSERT INTO comments (user_id, post_id, message) (3, 1, "Hi this is a text comment1");
