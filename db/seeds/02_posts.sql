d5110276fbef6ff2345b7dba3fd7c07781a51f3b
ssangbomb <ssangbomb@gmail.com>
2023-05-06 22:35:14 -0700

    revert

---
+++ db/seeds/02_posts.sql
@@ -0,0 +1,12 @@
+INSERT INTO posts (user_id, topic, title, description, external_url, rating, likes, date) VALUES (1, 'cooking', 'How to make branch', 'This is how to make it', 'https://www.foodnetwork.com/recipes/food-network-kitchen/spaghetti-with-oil-and-garlic-aglio-et-olio-recipe-1944538', 0, 0, NOW());
+
+INSERT INTO posts (user_id, topic, title, description, external_url, rating, likes, date) VALUES (2, 'sports', 'How to do wakeborad', 'This is how to do it', 'https://www.youtube.com/watch?v=F8Vosdtdc1A', 0, 0, NOW());
+
+INSERT INTO posts (user_id, topic, title, description, external_url, rating, likes, date) VALUES (4, 'cooking', 'How to make egg bendict', 'This is how to make it', 'https://www.simplyrecipes.com/recipes/eggs_benedict/', 0, 0, NOW());
+
+INSERT INTO posts (user_id, topic, title, description, external_url, rating, likes, date) VALUES (1, 'baby', 'How to putb a baby to sleep', 'This is how you do it', 'https://www.youtube.com/watch?v=EqrQIKRZa4w', 0, 0, NOW());
+
+INSERT INTO posts (user_id, topic, title, description, external_url, rating, likes, date) VALUES (4, 'game', 'How to play as jungler on league of legend', 'This is how you do it', 'https://gamerant.com/league-legends-beginner-jungle-tips/', 0, 0, NOW());
+
+
+
