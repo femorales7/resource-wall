c2c4c64f2a8c427897b52bb1e14ff411b32c3c52
ssangbomb <ssangbomb@gmail.com>
2023-05-08 23:52:46 -0700

    add more css on post page

--- db/seeds/02_posts.sql
+++ db/seeds/02_posts.sql
@@ -6,7 +6,7 @@ INSERT INTO posts (user_id, topic, title, description, external_url, rating, lik
 
 INSERT INTO posts (user_id, topic, title, description, external_url, rating, likes, date) VALUES (1, 'baby', 'How to putb a baby to sleep', 'This is how you do it', 'https://www.youtube.com/watch?v=EqrQIKRZa4w', 0, 0, NOW());
 
-INSERT INTO posts (user_id, topic, title, description, external_url, rating, likes, date) VALUES (4, 'game', 'How to play as jungler on league of legend', 'This is how you do it', 'https://gamerant.com/league-legends-beginner-jungle-tips/', 0, 0, NOW());
+INSERT INTO posts (user_id, topic, title, description, external_url, rating, likes, date) VALUES (4, 'game','How to play as jungler on league of legend', 'This is how you do it', 'https://gamerant.com/league-legends-beginner-jungle-tips/', 0, 0, NOW());
 
 
 
