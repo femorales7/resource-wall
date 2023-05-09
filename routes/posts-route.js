da83a5a1b80ea4db9bbc18d74a91d574d4bb44b9
ssangbomb <ssangbomb@gmail.com>
2023-05-08 20:32:12 -0700

    working on post page

--- routes/posts-route.js
+++ routes/posts-route.js
@@ -4,20 +4,24 @@ const express = require('express');
 const router  = express.Router();
 
 const postQuerys = require('../db/queries/getallposts');
-const onePostQuerys = require('../db/queries/onepost');
+const onePostQuerys = require('../db/queries/onepostwithcomment');
+
+
 
 router.get('/:id', (req, res) => {
-  onePostQuerys.onePost(req.params.id)
+  onePostQuerys.onePostWithcomment(req.params.id)
   .then((posts) => {
     const templateVars = { posts : posts };
     console.log('templateVars' ,templateVars);
-    res.render('post', templateVars);
+    return res.render('post', templateVars);
   })
   .catch((err) => {
     console.error(err);
     res.send(err);
   })
 });
+
+
 
 // router.get('/', (req, res) => {
 //   postQuerys.getAllPosts(10)
