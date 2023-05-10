70065adeba7bc2719cbe50a1f4702941c3f06022
Your Name <your@email.com>
2023-05-09 23:08:41 -0600

    add css for newPost

---
+++ db/queries/addNewComment.js
@@ -0,0 +1,10 @@
+const db = require('../connection');
+
+//adding new comment
+const addNewComment = (comment) => {
+  const query = `INSERT INTO comments (user_id, post_id, message)`
+
+
+
+
+}
--- db/seeds/02_posts.sql
+++ db/seeds/02_posts.sql
@@ -1,3 +1,11 @@
+INSERT INTO posts (user_id, topic, title, description, external_url, url_image, rating, likes, date) VALUES (3, 'game','How to play as jungler on league of legend', 'This is how you do it', 'https://gamerant.com/league-legends-beginner-jungle-tips/', 'https://www.gamespot.com/a/uploads/scale_medium/536/5360430/3064592-square%20league%20of%20legends%20box%20art.jpg',0, 0, NOW());
+
+INSERT INTO posts (user_id, topic, title, description, external_url, url_image, rating, likes, date) VALUES (4, 'game','Game of dragons', 'This is how you do it', 'https://go3.tv/en/tvod/how-to-train-your-dragon-2,vod-4855541', 'https://allears.net/wp-content/uploads/2023/02/dreamworks-how-to-train-your-dragon.png',5, 1, NOW());
+
+INSERT INTO posts (user_id, topic, title, description, external_url, url_image, rating, likes, date) VALUES (2, 'Movies','Harry Potter', 'HARRY POTTER AND THE DEATHLY HALLOWS: PART 2', 'https://go3.tv/en/tvod/harry-potter-and-the-deathly-hallows-part-2,vod-1601640', 'https://m.media-amazon.com/images/I/81l+vWFMdFL._AC_SL1500_.jpg',5, 1, NOW());
+
+INSERT INTO posts (user_id, topic, title, description, external_url, url_image, rating, likes, date) VALUES (4, 'Movies','Finding Jesus', 'Two friends, Muggles and Joy, spread faith and hope in a number of isolated encounters which reinforce basic biblical values.', 'https://www.imdb.com/title/tt12981810/', 'https://m.media-amazon.com/images/M/MV5BNjEzMDYxMTUtNzEwNS00YzJiLWEyY2MtOTA3MTk3OWFiNjhiXkEyXkFqcGdeQXVyMTEzMjQzMDM1._V1_.jpg',5, 1, NOW());
+
 INSERT INTO posts (user_id, topic, title, description, external_url, url_image,rating, likes, date) VALUES (1, 'cooking', 'How to make oil pasta', 'This is how to make it', 'https://www.foodnetwork.com/recipes/food-network-kitchen/spaghetti-with-oil-and-garlic-aglio-et-olio-recipe-1944538', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2017/2/22/0/FNK_spaghetti-with-oil-and-garlic_s4x3.jpg.rend.hgtvcom.1280.720.suffix/1487778382137.jpeg',0 ,0, NOW());
 
 INSERT INTO posts (user_id, topic, title, description, external_url, url_image,rating, likes, date) VALUES (2, 'sports', 'How to do wakeborad', 'This is how to do it', 'https://www.youtube.com/watch?v=F8Vosdtdc1A', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfXGtF02m8fPXDgHy2zCWR80jQ9svzZhwjsQ&usqp=CAU',0, 0, NOW());
@@ -5,12 +13,13 @@ INSERT INTO posts (user_id, topic, title, description, external_url, url_image,r
 INSERT INTO posts (user_id, topic, title, description, external_url, url_image, rating, likes, date) VALUES (4, 'cooking', 'How to make egg bendict', 'This is how to make it', 'https://www.simplyrecipes.com/recipes/eggs_benedict/', 'https://www.simplyrecipes.com/thmb/ah7pK0NFyMYJ-QJgT1HDEP3-AW0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Eggs-Benedict-LEAD-1-17de21a2975f4dd2bf339c0f5ec45c66.jpg',0, 0, NOW());
 
 INSERT INTO posts (user_id, topic, title, description, external_url, url_image,rating, likes, date) VALUES (1, 'baby', 'How to putb a baby to sleep', 'This is how you do it', 'https://www.youtube.com/watch?v=EqrQIKRZa4w', 'https://www.parents.com/thmb/8pD3F4tCMss1tYiupIbTMUDg3V4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-135538251-2000-36fa3378fc014ce2b81526dd22aa488f.jpg',0, 0, NOW());
-<<<<<<< HEAD
-=======
 
-INSERT INTO posts (user_id, topic, title, description, external_url, url_image, rating, likes, date) VALUES (4, 'game','How to play as jungler on league of legend', 'This is how you do it', 'https://gamerant.com/league-legends-beginner-jungle-tips/', 'https://static.wikia.nocookie.net/leagueoflegends/images/9/95/Jungle_camps_SR.jpg/revision/latest/scale-to-width-down/1000?cb=20140701232747',0, 0, NOW());
->>>>>>> df5fc0c84ab26b48efec76b3b34d6810e860e959
+INSERT INTO posts (user_id, topic, title, description, external_url, url_image, rating, likes, date) VALUES (3, 'game','How to play as jungler on league of legend', 'This is how you do it', 'https://gamerant.com/league-legends-beginner-jungle-tips/', 'https://www.gamespot.com/a/uploads/scale_medium/536/5360430/3064592-square%20league%20of%20legends%20box%20art.jpg',0, 0, NOW());
 
-INSERT INTO posts (user_id, topic, title, description, external_url, url_image, rating, likes, date) VALUES (4, 'game','How to play as jungler on league of legend', 'This is how you do it', 'https://gamerant.com/league-legends-beginner-jungle-tips/', 'https://static.wikia.nocookie.net/leagueoflegends/images/9/95/Jungle_camps_SR.jpg/revision/latest/scale-to-width-down/1000?cb=20140701232747',0, 0, NOW());
+INSERT INTO posts (user_id, topic, title, description, external_url, url_image, rating, likes, date) VALUES (1, 'game','Call of Duty', 'Harness your competitive spirit with the return of two fan-favorite modes, Gunfight and Cranked. Drop into four new maps at launch, including two large-sized Battle Maps for Ground War and Invasion. And earn 100+ Tiers of new content in the new Battle Pass!', 'https://www.xbox.com/en-CA/games/call-of-duty-modern-warfare-ii', 'https://i5.walmartimages.ca/images/Large/546/178/6000205546178.jpg',5, 1, NOW());
+
+INSERT INTO posts (user_id, topic, title, description, external_url, url_image, rating, likes, date) VALUES (2, 'Movies','Evil Dead Rise', 'A twisted tale of two estranged sisters whose reunion is cut short by the rise of flesh-possessing demons, thrusting them into a primal battle for survival ', 'https://www.imdb.com/title/tt13345606/', 'https://m.media-amazon.com/images/M/MV5BMmZiN2VmMjktZDE5OC00ZWRmLWFlMmEtYWViMTY4NjM3ZmNkXkEyXkFqcGdeQXVyMTI2MTc2ODM3._V1_.jpg',5, 1, NOW());
+
+INSERT INTO posts (user_id, topic, title, description, external_url, url_image, rating, likes, date) VALUES (4, 'Movies','The Last of Us', 'The Last of Us is an American post-apocalyptic drama television series created by Craig Mazin and Neil Druckmann for HBO. Based on the 2013 video game ', 'https://en.wikipedia.org/wiki/The_Last_of_Us_(TV_series)', 'https://m.media-amazon.com/images/M/MV5BZGUzYTI3M2EtZmM0Yy00NGUyLWI4ODEtN2Q3ZGJlYzhhZjU3XkEyXkFqcGdeQXVyNTM0OTY1OQ@@._V1_.jpg',5, 1, NOW());
 
 
