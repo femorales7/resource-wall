36eb281e56e3fa0d51411167127efd16bd5640eb
ssangbomb <ssangbomb@gmail.com>
2023-05-10 11:23:55 -0700

    updating profile edit

--- routes/users-route.js
+++ routes/users-route.js
@@ -14,56 +14,39 @@ const userQuerys = require('../db/queries/getuserwithemail');
 const { render } = require('ejs');
 
 
-router.post('/users', (req, res) => {
-  console.log(req.body);
-  const username = req.body.username;
-  const password = req.body.password;
-  if(!username || !password){
-    res.status(400).send('Please Provide an email AND a password');
-  }
-  userQuerys.getUserWithEmail(req.body.username)
-  .then((user) => {
-    console.log(user);
-    res.cookie('user_id', user.id);
-    const templateVars = { user : user };
-    console.log('templateVars' ,templateVars);
-    return res.render('index', templateVars );
-
-  })
-
-
-})
-
-// router.get('/', (req, res) => {
-//   userQuerys.getUsers()
-//   .then((users) => {
-//     console.log(users);
-//     res.json(users);
-//   });
-// });
-
-// router.get('/login/:id', (req, res) => {
-//   res.cookie('user_id', req.params.id);
-//   res.redirect('/');
-// });
-
-
+// router.post('/users', (req, res) => {
+//   console.log(req.body);
+//   const username = req.body.username;
+//   const password = req.body.password;
+//   if(!username || !password){
+//     res.status(400).send('Please Provide an email AND a password');
+//   }
+//   userQuerys.getUserWithEmail(req.body.username)
+//   .then((user) => {
+//     console.log(user);
+//     res.cookie('user_id', user.id);
+//     const templateVars = { user : user };
+//     console.log('templateVars' ,templateVars);
+//     return res.render('index', templateVars );
+//   })
+// })
 
 router.get('/', (req, res) => {
-  console.log('cookie', req.cookies);
   const userId = req.cookies['user_id'];
-  console.log('userid', userId);
   getUserWithIdQuerys.getUserWithId(userId)
   .then((user) => {
-    console.log(user);
-    res.json(user);
+    const templateVars = { user : user};
+    console.log(templateVars);
+    res.render('user', templateVars);
   });
 });
 
 router.get('/', (req, res) => {
   const userId = req.cookies;
   console.log(userId);
-})
+});
+
+router.post("/:")
 
 
 module.exports = router;
