d6161752ccd19200207845793fb940e162ac0511
ssangbomb <ssangbomb@gmail.com>
2023-05-11 18:09:21 -0700

    add rating, likes table

---
+++ db/seeds/04_ratings.sql
@@ -0,0 +1,5 @@
+INSERT INTO ratings (user_id, post_id, rating) VALUES (1, 1, 9);
+
+INSERT INTO ratings (user_id, post_id, rating) VALUES (2, 2, 6);
+
+INSERT INTO ratings (user_id, post_id, rating) VALUES (3, 1, 8);
